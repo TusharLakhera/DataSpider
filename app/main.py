2729c5e19cdc999ecb973fa0c8b0e3d0c00b9ad1
Tushar Lakhera <tusharlakhera18@gmail.com>
2024-05-13 18:50:02 +0530

    logging removed and host changed in redis

--- app/main.py
+++ app/main.py
@@ -14,11 +14,10 @@ from .cache import RedisCache
 app = FastAPI()
 
 # Create a Redis client
-redis_client = redis.Redis(host='localhost', port=6379, db=0)
+redis_client = redis.Redis(host='redis', port=6379, db=0)
 
 @app.get("/scrape", dependencies=[Depends(authenticate)], response_model=List[Product])
 async def scrape_products(max_pages: int = None, proxy: str = None):
-    print("**************I am here")
     storage = JSONStorage("data/products.json")
     notification = ConsoleNotification()
     cache = RedisCache(redis_client)
